4eca1fe10cc3cac626b786fd749e20ad57d26343
Akhmir-office <168223656+Akhmir-office@users.noreply.github.com>
2024-05-01 23:09:27 +0100

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -2,12 +2,12 @@ var handler = async (m, {conn, usedPrefix, text, command}) => {
   
 let hash = text
 if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-if (!hash) return conn.reply(m.chat, `🎌 *Ingrese el código de la asignación*\n\nUtilice !listcmd`, m, fake, )
+if (!hash) return conn.reply(m.chat, `🎌 *أدخل رمز المهمة*\n\nUtilice !listcmd`, m, fake, )
 
 let sticker = global.db.data.sticker
-if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '🚩 *¡Solo mi propietario puede usar esta función!*', m, fake, )
+if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, '🚩 *¡يمكن لمالكي فقط استخدام هذه الميزة!*', m, fake, )
 delete sticker[hash]
-conn.reply(m.chat, `✅ *Eliminado correctamente*`, m, fake, )
+conn.reply(m.chat, `✅ *تمت الإزالة بنجاح*`, m, fake, )
 
 }
 handler.tags = ['database']
