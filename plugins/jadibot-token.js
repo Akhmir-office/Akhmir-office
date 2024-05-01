88350e59b15e4d7aef297083446bd609320eee63
Akhmir-office <168223656+Akhmir-office@users.noreply.github.com>
2024-05-01 23:17:30 +0100

    Update jadibot-token.js

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -6,10 +6,10 @@ const user = m.sender.split('@')[0]
 if (fs.existsSync('./jadibts/' + user + '/creds.json')) {
 let token = Buffer.from(fs.readFileSync('./jadibts/' + user + '/creds.json'), 'utf-8').toString('base64')
 
-await conn.reply(m.chat, `🎌 *El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie*\n\nTu token es:`, m, fake, )
+await conn.reply(m.chat, `🎌 *يسمح لك الرمز المميز بتسجيل الدخول إلى برامج الروبوت الأخرى، ونوصي بعدم مشاركته مع أي شخص*\n\nTu token es:`, m, fake, )
 await conn.reply(m.chat, token, m, fake, )
 } else {
-await conn.reply(m.chat, `🚩 *No tienes ningun token activo, usa !jadibot para crear uno*`, m, fake, )
+await conn.reply(m.chat, `🚩 *ليس لديك أي رموز مميزة نشطة، استخدم !jadibot لإنشاء واحدة*`, m, fake, )
 }
 
 }
