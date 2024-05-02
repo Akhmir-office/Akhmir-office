a4963b4579ff748f1e7cf8125c1a3af0eed0ce01
Akhmir-office <168223656+Akhmir-office@users.noreply.github.com>
2024-05-02 01:14:00 +0100

    Update descargas-gitclone.js

--- plugins/descargas-gitclone.js
+++ plugins/descargas-gitclone.js
@@ -3,14 +3,14 @@ import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 var handler = async (m, { args, usedPrefix, command }) => {
 
-if (!args[0]) return conn.reply(m.chat, `🎌 *Enviar comando:*\n!gitclone repositorio\n\nEjemplo, !gitclone ${md}`, m, fake, )
-if (!regex.test(args[0])) conn.reply(m.chat, `🚩 *No es un enlace válido*`, m, fake, )
+if (!args[0]) return conn.reply(m.chat, `🎌 *إرسال الأمر:*\n!gitclone repositorio\n\nEjemplo, !gitclone ${md}`, m, fake, )
+if (!regex.test(args[0])) conn.reply(m.chat, `🚩 *إنه ليس رابطًا صالحًا*`, m, fake, )
 
 let [_, user, repo] = args[0].match(regex) || []
 repo = repo.replace(/.git$/, '')
 let url = `https://api.github.com/repos/${user}/${repo}/zipball`
 let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-conn.reply(m.chat, `⏰ Espere un momento`, m, fake, )
+conn.reply(m.chat, `⏰ انتظر لحظة`, m, fake, )
 conn.sendFile(m.chat, url, filename, null, m)
 
 }
